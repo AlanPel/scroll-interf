80b8f314c0299f33c223654faab8382a108e6c0d
AlanPel <83054641+AlanPel@users.noreply.github.com>
2023-04-17 19:53:00 +0200

    Update useSwapCallback.tsx

--- src/hooks/useSwapCallback.tsx
+++ src/hooks/useSwapCallback.tsx
@@ -1,11 +1,16 @@
 import { Trade } from '@uniswap/router-sdk'
 import { Currency, Percent, TradeType } from '@uniswap/sdk-core'
-import { PermitSignature } from 'hooks/usePermitAllowance'
-import { useMemo } from 'react'
+import { useWeb3React } from '@web3-react/core'
+import { usePermit2Enabled } from 'featureFlags/flags/permit2'
+import { SwapCallbackState, useSwapCallback as useLibSwapCallBack } from 'lib/hooks/swap/useSwapCallback'
+import { ReactNode, useMemo } from 'react'
 
 import { useTransactionAdder } from '../state/transactions/hooks'
 import { TransactionType } from '../state/transactions/types'
 import { currencyId } from '../utils/currencyId'
+import useENS from './useENS'
+import { SignatureData } from './useERC20Permit'
+import { Permit } from './usePermit2'
 import useTransactionDeadline from './useTransactionDeadline'
 import { useUniversalRouterSwapCallback } from './useUniversalRouter'
 
@@ -13,20 +18,38 @@ import { useUniversalRouterSwapCallback } from './useUniversalRouter'
 // and the user has approved the slippage adjusted input amount for the trade
 export function useSwapCallback(
   trade: Trade<Currency, Currency, TradeType> | undefined, // trade to execute, required
-  fiatValues: { amountIn: number | undefined; amountOut: number | undefined }, // usd values for amount in and out, logged for analytics
   allowedSlippage: Percent, // in bips
-  permitSignature: PermitSignature | undefined
-): { callback: null | (() => Promise<string>) } {
+  recipientAddressOrName: string | null, // the ENS name or address of the recipient of the trade, or null if swap should be returned to sender
+  signatureData: SignatureData | undefined | null,
+  permit: Permit | undefined
+): { state: SwapCallbackState; callback: null | (() => Promise<string>); error: ReactNode | null } {
+  const { account } = useWeb3React()
+
   const deadline = useTransactionDeadline()
 
   const addTransaction = useTransactionAdder()
 
-  const universalRouterSwapCallback = useUniversalRouterSwapCallback(trade, fiatValues, {
+  const { address: recipientAddress } = useENS(recipientAddressOrName)
+  const recipient = recipientAddressOrName === null ? account : recipientAddress
+
+  const permit2Enabled = usePermit2Enabled()
+  const {
+    state,
+    callback: libCallback,
+    error,
+  } = useLibSwapCallBack({
+    trade: permit2Enabled ? undefined : trade,
+    allowedSlippage,
+    recipientAddressOrName: recipient,
+    signatureData,
+    deadline,
+  })
+  const universalRouterSwapCallback = useUniversalRouterSwapCallback(permit2Enabled ? trade : undefined, {
     slippageTolerance: allowedSlippage,
     deadline,
-    permit: permitSignature,
+    permit: permit?.signature,
   })
-  const swapCallback = universalRouterSwapCallback
+  const swapCallback = permit2Enabled ? universalRouterSwapCallback : libCallback
 
   const callback = useMemo(() => {
     if (!trade || !swapCallback) return null
@@ -36,29 +59,31 @@ export function useSwapCallback(
           response,
           trade.tradeType === TradeType.EXACT_INPUT
             ? {
-                type: TransactionType.SWAP,
-                tradeType: TradeType.EXACT_INPUT,
-                inputCurrencyId: currencyId(trade.inputAmount.currency),
-                inputCurrencyAmountRaw: trade.inputAmount.quotient.toString(),
-                expectedOutputCurrencyAmountRaw: trade.outputAmount.quotient.toString(),
-                outputCurrencyId: currencyId(trade.outputAmount.currency),
-                minimumOutputCurrencyAmountRaw: trade.minimumAmountOut(allowedSlippage).quotient.toString(),
-              }
+              type: TransactionType.SWAP,
+              tradeType: TradeType.EXACT_INPUT,
+              inputCurrencyId: currencyId(trade.inputAmount.currency),
+              inputCurrencyAmountRaw: trade.inputAmount.quotient.toString(),
+              expectedOutputCurrencyAmountRaw: trade.outputAmount.quotient.toString(),
+              outputCurrencyId: currencyId(trade.outputAmount.currency),
+              minimumOutputCurrencyAmountRaw: trade.minimumAmountOut(allowedSlippage).quotient.toString(),
+            }
             : {
-                type: TransactionType.SWAP,
-                tradeType: TradeType.EXACT_OUTPUT,
-                inputCurrencyId: currencyId(trade.inputAmount.currency),
-                maximumInputCurrencyAmountRaw: trade.maximumAmountIn(allowedSlippage).quotient.toString(),
-                outputCurrencyId: currencyId(trade.outputAmount.currency),
-                outputCurrencyAmountRaw: trade.outputAmount.quotient.toString(),
-                expectedInputCurrencyAmountRaw: trade.inputAmount.quotient.toString(),
-              }
+              type: TransactionType.SWAP,
+              tradeType: TradeType.EXACT_OUTPUT,
+              inputCurrencyId: currencyId(trade.inputAmount.currency),
+              maximumInputCurrencyAmountRaw: trade.maximumAmountIn(allowedSlippage).quotient.toString(),
+              outputCurrencyId: currencyId(trade.outputAmount.currency),
+              outputCurrencyAmountRaw: trade.outputAmount.quotient.toString(),
+              expectedInputCurrencyAmountRaw: trade.inputAmount.quotient.toString(),
+            }
         )
         return response.hash
       })
   }, [addTransaction, allowedSlippage, swapCallback, trade])
 
   return {
+    state,
     callback,
+    error,
   }
 }
