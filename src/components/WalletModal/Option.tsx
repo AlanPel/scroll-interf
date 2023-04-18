c1ee592fc1ce13c56a4ddb97a781327ce6e4d187
AlanPel <83054641+AlanPel@users.noreply.github.com>
2023-04-18 11:05:10 +0200

    wllet od

--- src/components/WalletModal/Option.tsx
+++ src/components/WalletModal/Option.tsx
@@ -1,99 +1,149 @@
 import { TraceEvent } from '@uniswap/analytics'
-import { BrowserEvent, InterfaceElementName, InterfaceEventName } from '@uniswap/analytics-events'
-import Loader from 'components/Icons/LoadingSpinner'
-import { Connection, ConnectionType } from 'connection'
+import { BrowserEvent, ElementName, EventName } from '@uniswap/analytics-events'
+import React from 'react'
+import { Check } from 'react-feather'
 import styled from 'styled-components/macro'
-import { useIsDarkMode } from 'theme/components/ThemeToggle'
 import { flexColumnNoWrap, flexRowNoWrap } from 'theme/styles'
 
-import NewBadge from './NewBadge'
+import { ExternalLink } from '../../theme'
 
-const OptionCardLeft = styled.div`
-  ${flexColumnNoWrap};
-  flex-direction: row;
-  align-items: center;
+const InfoCard = styled.button<{ isActive?: boolean }>`
+  background-color: ${({ theme }) => theme.backgroundInteractive};
+  padding: 1rem;
+  outline: none;
+  border: 1px solid;
+  border-radius: 12px;
+  width: 100% !important;
+  &:focus {
+    background-color: ${({ theme }) => theme.hoverState};
+  }
+  border-color: ${({ theme, isActive }) => (isActive ? theme.accentActive : 'transparent')};
 `
 
-const OptionCardClickable = styled.button<{ isActive?: boolean; clickable?: boolean }>`
-  background-color: ${({ theme }) => theme.backgroundModule};
-  width: 100% !important;
-  border-color: ${({ theme, isActive }) => (isActive ? theme.accentActive : 'transparent')};
+const CheckIcon = styled(Check)`
+  ${flexColumnNoWrap};
+  height: 20px;
+  width: 20px;
+  align-items: center;
+  justify-content: center;
+  color: ${({ theme }) => theme.accentAction};
+  ${({ theme }) => theme.deprecated_mediaWidth.deprecated_upToMedium`
+    align-items: flex-end;
+  `};
+`
 
+const OptionCard = styled(InfoCard as any)`
   display: flex;
   flex-direction: row;
   align-items: center;
   justify-content: space-between;
   margin-top: 2rem;
   padding: 1rem;
+`
 
+const OptionCardLeft = styled.div`
+  ${flexColumnNoWrap};
+  justify-content: center;
+  height: 100%;
+`
+
+const OptionCardClickable = styled(OptionCard as any)<{
+  active?: boolean
+  clickable?: boolean
+}>`
   margin-top: 0;
-  transition: ${({ theme }) => theme.transition.duration.fast};
-  opacity: ${({ disabled }) => (disabled ? '0.5' : '1')};
+  border: ${({ active, theme }) => active && `1px solid ${theme.accentActive}`};
   &:hover {
     cursor: ${({ clickable }) => clickable && 'pointer'};
-    background-color: ${({ theme, clickable }) => clickable && theme.hoverState};
+    background-color: ${({ theme }) => theme.hoverState};
   }
-  &:focus {
-    background-color: ${({ theme, clickable }) => clickable && theme.hoverState};
-  }
+  opacity: ${({ disabled }) => (disabled ? '0.5' : '1')};
 `
 
 const HeaderText = styled.div`
   ${flexRowNoWrap};
   align-items: center;
   justify-content: center;
-  color: ${(props) => (props.color === 'blue' ? ({ theme }) => theme.accentAction : ({ theme }) => theme.textPrimary)};
+  color: ${(props) =>
+    props.color === 'blue' ? ({ theme }) => theme.deprecated_primary1 : ({ theme }) => theme.deprecated_text1};
   font-size: 16px;
   font-weight: 600;
-  padding: 0 8px;
 `
 
-const IconWrapper = styled.div`
+const SubHeader = styled.div`
+  color: ${({ theme }) => theme.deprecated_text1};
+  margin-top: 10px;
+  font-size: 12px;
+`
+
+const IconWrapper = styled.div<{ size?: number | null }>`
   ${flexColumnNoWrap};
   align-items: center;
   justify-content: center;
+  padding-right: 12px;
   & > img,
   span {
-    height: 40px;
-    width: 40px;
+    height: ${({ size }) => (size ? size + 'px' : '28px')};
+    width: ${({ size }) => (size ? size + 'px' : '28px')};
   }
   ${({ theme }) => theme.deprecated_mediaWidth.deprecated_upToMedium`
     align-items: flex-end;
   `};
 `
 
-type OptionProps = {
-  connection: Connection
-  activate: () => void
-  pendingConnectionType?: ConnectionType
-}
-export default function Option({ connection, pendingConnectionType, activate }: OptionProps) {
-  const isPending = pendingConnectionType === connection.type
-  const isDarkMode = useIsDarkMode()
+export default function Option({
+  link = null,
+  clickable = true,
+  size,
+  onClick = null,
+  color,
+  header,
+  subheader,
+  icon,
+  isActive = false,
+  id,
+}: {
+  link?: string | null
+  clickable?: boolean
+  size?: number | null
+  onClick?: null | (() => void)
+  color: string
+  header: React.ReactNode
+  subheader?: React.ReactNode
+  icon: string
+  isActive?: boolean
+  id: string
+}) {
   const content = (
     <TraceEvent
       events={[BrowserEvent.onClick]}
-      name={InterfaceEventName.WALLET_SELECTED}
-      properties={{ wallet_type: connection.getName() }}
-      element={InterfaceElementName.WALLET_TYPE_OPTION}
+      name={EventName.WALLET_SELECTED}
+      properties={{ wallet_type: header }}
+      element={ElementName.WALLET_TYPE_OPTION}
     >
       <OptionCardClickable
-        onClick={!pendingConnectionType ? activate : undefined}
-        clickable={!pendingConnectionType}
-        disabled={Boolean(!isPending && !!pendingConnectionType)}
+        id={id}
+        onClick={onClick}
+        clickable={clickable && !isActive}
+        active={isActive}
         data-testid="wallet-modal-option"
       >
         <OptionCardLeft>
-          <IconWrapper>
-            <img src={connection.getIcon?.(isDarkMode)} alt="Icon" />
-          </IconWrapper>
-          <HeaderText>{connection.getName()}</HeaderText>
-          {connection.isNew && <NewBadge />}
+          <HeaderText color={color}>
+            <IconWrapper size={size}>
+              <img src={icon} alt="Icon" />
+            </IconWrapper>
+            {header}
+          </HeaderText>
+          {subheader && <SubHeader>{subheader}</SubHeader>}
         </OptionCardLeft>
-        {isPending && <Loader />}
+        {isActive && <CheckIcon />}
       </OptionCardClickable>
     </TraceEvent>
   )
+  if (link) {
+    return <ExternalLink href={link}>{content}</ExternalLink>
+  }
 
   return content
 }
